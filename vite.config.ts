e84a583ddf66b7f7d30985cb36e4c482309c41b2
TheOneMicky <njagwamicky@gmail.com>
2025-04-24 12:29:58 +0300

    Verified initial code commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/weather-app/", // Add this line - replace 'weather-app' with your repo name
+  base: "/WeatherApp/",
   server: {
     host: "::",
     port: 8080,
